6f555bccba8a90208b93eabe97815f847cdd86da
jumproper <dylanmplummer@gmail.com>
2018-10-29 23:04:13 -0400

    Fix Keras implementation with proper reshaping

--- seq_cnn_keras.py
+++ seq_cnn_keras.py
@@ -5,16 +5,17 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import StratifiedShuffleSplit
 from keras.models import Sequential
-from keras.layers import Dense, Activation, Flatten, Convolution1D, Dropout
+from keras.layers import Dense, Activation, Flatten, Dropout, Conv1D, AveragePooling1D
 from keras.optimizers import SGD
 from keras.utils import np_utils
 import data_helpers as dhrt
 
 
 # Network Parameters
+learning_rate = 0.01
 num_classes = 2
 num_features = 372
-batch_size = 16
+batch_size = 32
 
 # load data
 x_rt, y_rt = dhrt.load_data_and_labels('h3.pos','h3.neg')
@@ -32,6 +33,19 @@ l_x_rt = len(y_rt)
 vocab_size = len(vocab_processor.vocabulary_)
 
 print( "Vocab Size: ", vocab_size)
+num_classes = 2
+num_filters = [16, 8]
+
+region_size = 51 #can be considered as filter size but not really
+
+#this value has to be selected based on max_document_length and region_size
+#here I ensured that max_docu length is even and region size in odd
+#so division by 2 is possible
+num_pooled = (max_document_length-region_size+1)/2
+
+print( "Pool Size: ", num_pooled)
+
+
 #np.random.seed(10) #for 'reproducible research' B-)
 #^comment this line out to evaluate the model more thoroughly
 #in case this line is commented out, standard cross validation is assumed
@@ -56,29 +70,44 @@ for train_index, valid_index in sss.split(scaled_train, y_rt_shuffled):
     y_train, y_valid = y_rt_shuffled[train_index], y_rt_shuffled[valid_index]
 
 # reshape train data
-X_train_r = np.zeros((len(X_train), num_features, 3))
+print('X_train',X_train.shape)
+X_train_r = np.zeros((X_train.shape[0], X_train.shape[1], batch_size))
+for i in range(batch_size):
+    X_train_r[:, :, i] = X_train
+print(X_train_r.shape)
+X_valid_r = np.zeros((X_valid.shape[0], X_valid.shape[1], batch_size, ))
+for i in range(batch_size):
+    X_valid_r[:, :, i] = X_valid
 
-# reshape validation data
-X_valid_r = np.zeros((len(X_valid), num_features, 3))
+#y_train = np_utils.to_categorical(y_train, num_classes)
+#y_valid = np_utils.to_categorical(y_train, num_classes)
 
+
+filter_length = vocab_size*region_size
+sentence_length = x_rt_proc.shape[1]*vocab_size
+cnn_filter_shape = [filter_length, 1, 1, num_filters[0]]
+pool_stride = [1,int((x_rt_proc.shape[1]-region_size+1)/num_pooled),1,1]
 # Keras model with one Convolution1D layer
 # unfortunately more number of covnolutional layers, filters and filters lenght
 # don't give better accuracy
 model = Sequential()
-model.add(Convolution1D(nb_filter=512, filter_length=1, input_shape=(num_features, 3)))
+model.add(Conv1D(nb_filter=num_filters[0], filter_length=filter_length, input_shape=X_train_r.shape[1:]))
 model.add(Activation('relu'))
+#model.add(Conv1D(nb_filter=num_filters[1], filter_length=filter_length))
+#model.add(Activation('relu'))
+#model.add(Flatten())
+model.add(AveragePooling1D(pool_size=int(num_pooled), padding='VALID'))
+model.add(Activation('relu'))
+model.add(Dropout(0.3))
+model.add(Dense(2048, activation='relu'))
+model.add(Dense(1024, activation='relu'))
 model.add(Flatten())
-model.add(Dropout(0.4))
-model.add(Dense(256, activation='relu'))
-model.add(Dense(256, activation='relu'))
 model.add(Dense(num_classes))
 model.add(Activation('softmax'))
+print(model.summary())
 
-y_rt_train = np_utils.to_categorical(y_train, num_classes)
-y_rt_val = np_utils.to_categorical(y_train, num_classes)
-
-sgd = SGD(lr=0.01, nesterov=True, decay=1e-6, momentum=0.9)
-model.compile(loss='categorical_crossentropy',optimizer=sgd,metrics=['accuracy'])
+sgd = SGD(lr=learning_rate, nesterov=True, decay=1e-6, momentum=0.9)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 
 nb_epoch = 15
 model.fit(X_train_r, y_train, nb_epoch=nb_epoch, validation_data=(X_valid_r, y_valid), batch_size=batch_size)
