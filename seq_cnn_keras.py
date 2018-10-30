912ded1a4c0a326227b381c83b1223f40ac3eec0
jumproper <dylanmplummer@gmail.com>
2018-10-30 12:52:48 -0400

    Add second Conv1D layer and training visualization

--- seq_cnn_keras.py
+++ seq_cnn_keras.py
@@ -6,16 +6,18 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import StratifiedShuffleSplit
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Flatten, Dropout, Conv1D, AveragePooling1D
-from keras.optimizers import SGD
+from keras.optimizers import SGD, Adam
 from keras.utils import np_utils
+import matplotlib.pyplot as plt
 import data_helpers as dhrt
 
 
 # Network Parameters
-learning_rate = 0.01
+learning_rate = 0.001
 num_classes = 2
 num_features = 372
 batch_size = 32
+nb_epoch = 16
 
 # load data
 x_rt, y_rt = dhrt.load_data_and_labels('h3.pos','h3.neg')
@@ -34,7 +36,7 @@ vocab_size = len(vocab_processor.vocabulary_)
 
 print( "Vocab Size: ", vocab_size)
 num_classes = 2
-num_filters = [16, 8]
+num_filters = [16, 4]
 
 region_size = 51 #can be considered as filter size but not really
 
@@ -64,7 +66,7 @@ scaler = StandardScaler().fit(x_rt_shuffled)
 scaled_train = scaler.transform(x_rt_shuffled)
 
 # split train data into train and validation
-sss = StratifiedShuffleSplit(test_size=0.1, random_state=23)
+sss = StratifiedShuffleSplit(test_size=0.2, random_state=23)
 for train_index, valid_index in sss.split(scaled_train, y_rt_shuffled):
     X_train, X_valid = scaled_train[train_index], scaled_train[valid_index]
     y_train, y_valid = y_rt_shuffled[train_index], y_rt_shuffled[valid_index]
@@ -75,7 +77,7 @@ X_train_r = np.zeros((X_train.shape[0], X_train.shape[1], batch_size))
 for i in range(batch_size):
     X_train_r[:, :, i] = X_train
 print(X_train_r.shape)
-X_valid_r = np.zeros((X_valid.shape[0], X_valid.shape[1], batch_size, ))
+X_valid_r = np.zeros((X_valid.shape[0], X_valid.shape[1], batch_size))
 for i in range(batch_size):
     X_valid_r[:, :, i] = X_valid
 
@@ -90,12 +92,13 @@ pool_stride = [1,int((x_rt_proc.shape[1]-region_size+1)/num_pooled),1,1]
 # Keras model with one Convolution1D layer
 # unfortunately more number of covnolutional layers, filters and filters lenght
 # don't give better accuracy
+print('SHAPE?', X_train_r.shape[1:])
 model = Sequential()
+# Shape is (batch_size, sentence_length)
 model.add(Conv1D(nb_filter=num_filters[0], filter_length=filter_length, input_shape=X_train_r.shape[1:]))
 model.add(Activation('relu'))
-#model.add(Conv1D(nb_filter=num_filters[1], filter_length=filter_length))
-#model.add(Activation('relu'))
-#model.add(Flatten())
+model.add(Conv1D(nb_filter=num_filters[1], filter_length=3))
+model.add(Activation('relu'))
 model.add(AveragePooling1D(pool_size=int(num_pooled), padding='VALID'))
 model.add(Activation('relu'))
 model.add(Dropout(0.3))
@@ -106,8 +109,25 @@ model.add(Dense(num_classes))
 model.add(Activation('softmax'))
 print(model.summary())
 
+adam = Adam(lr=learning_rate)
 sgd = SGD(lr=learning_rate, nesterov=True, decay=1e-6, momentum=0.9)
-model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+model.compile(loss='categorical_crossentropy', optimizer=adam, metrics=['accuracy'])
 
-nb_epoch = 15
-model.fit(X_train_r, y_train, nb_epoch=nb_epoch, validation_data=(X_valid_r, y_valid), batch_size=batch_size)
+history = model.fit(X_train_r, y_train, nb_epoch=nb_epoch, validation_data=(X_valid_r, y_valid), batch_size=batch_size)
+print(history.history.keys())
+# summarize history for accuracy
+plt.plot(history.history['acc'])
+plt.plot(history.history['val_acc'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+# summarize history for loss
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
