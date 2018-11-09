6cc04033a6e0f4e2e1a183ca94fb6b6bbb07123a
jumproper <dylanmplummer@gmail.com>
2018-11-08 20:55:21 -0500

    Some strange shapes be among us

--- alignments.py
+++ alignments.py
@@ -9,6 +9,7 @@ from sklearn.model_selection import train_test_split
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Flatten, Dropout, Conv1D, AveragePooling1D, MaxPooling1D, LSTM, Bidirectional, BatchNormalization
 from keras.optimizers import SGD, Adam
+from keras.utils import np_utils
 import matplotlib.pyplot as plt
 from Bio import pairwise2
 
@@ -17,8 +18,8 @@ from Bio import pairwise2
 learning_rate = 0.001
 num_classes = 2
 num_features = 372
-batch_size = 16
-nb_epoch = 4
+batch_size = 4
+nb_epoch = 8
 hidden_size = 100
 num_sequences = 10
 num_classes = 2
@@ -37,39 +38,50 @@ def get_alignments(x, y, seq_i, seq_j, batch_size):
     :return: a set of pairwise alignments of the sets in x (cartesian product) x
     '''
     a = pairwise2.align.globalxx(x[seq_i], x[seq_j], one_alignment_only=True)[0]
-    align_x = np.array(list(a))[0:2]
+    align_x = np.array(list(a)[0:2])
     if np.array_equal(y[seq_i], y[seq_j]):
-        align_y = np.array([1, 0])
+        align_y = np.array([1])
     else:
-        align_y = np.array([0, 1])
+        align_y = np.array([0])
     for i in range(seq_i + 1, seq_i + batch_size):
         for j in range(seq_j + 1, seq_j + batch_size):
             if i < j:
                 a = pairwise2.align.globalxx(x[i], x[j], one_alignment_only=True)[0]
-                align_x = np.vstack((align_x, np.array(list(a))[0:2]))
+                align_x = np.vstack((align_x, np.array(list(a)[0:2])))
                 if np.array_equal(y[i], y[j]):
-                    align_y = np.vstack((align_y, np.array([1, 0])))
+                    align_y = np.append(align_y, np.array([1]))
                 else:
-                    align_y = np.vstack((align_y, np.array([1, 0])))
+                    align_y = np.append(align_y, np.array([0]))
     return align_x, align_y
 
 
-def convert_base_pairs(x):
+def convert_base_pairs(x, y):
     lens = [len(seq) for alignment in x for seq in alignment]
     max_document_length = max(lens)
     if max_document_length % 2 != 0:
         max_document_length = max_document_length + 1
     print('Max seq length:', max_document_length)
+    print('Input shape:', x.shape)
     vocab_processor = learn.preprocessing.VocabularyProcessor(max_document_length)
     x_proc = []
-    for alignment in x:
+    y_proc = []
+    for i in range(len(x)):
         #print('Alignment shape:', alignment.shape)
-        proc = np.array(list(vocab_processor.transform(' '.join(alignment)[:-1])))
+        proc = np.array(list(vocab_processor.fit_transform(' '.join(x[i]))))
         scaler = StandardScaler().fit(np.float64(proc))
         proc = scaler.transform(np.float64(proc))
-        x_proc = np.append(x_proc, proc)
+        proc = np.reshape(proc, (2, -1))
+        if len(x_proc) == 0:
+            x_proc = proc
+            y_proc = y[i]
+        else:
+            x_proc = np.concatenate((x_proc, proc), axis=1)
+            y_proc = np.append(y_proc, y[i])
         #print('Resulting shape:', x_proc.shape, proc.shape)
-    return np.reshape(x_proc, (max_document_length, 2, x.shape[0]))
+    #print(x_proc)
+    x_proc = np.reshape(x_proc, (-1, 2, max_document_length))
+    y_proc = np_utils.to_categorical(y_proc, num_classes=num_classes)
+    return x_proc, y_proc
 
 
 def generate_batch(x, y):
@@ -78,7 +90,7 @@ def generate_batch(x, y):
             if i < j and (i + batch_size) < len(x) and (j + batch_size) < len(x):
                 align_x, align_y = (get_alignments(x, y, i, j, batch_size))
                 #align_y = np.reshape(align_y, (len(align_y)//2, 2))
-                align_x = convert_base_pairs(align_x)
+                align_x, align_y = convert_base_pairs(align_x, align_y)
                 #align_x = np.reshape(align_x, (align_x.shape[0], align_x.shape[2], 2))
                 #align_x = np.swapaxes(align_x, 1, 2)
                 print('Num Alignments', align_x.shape)
@@ -113,7 +125,7 @@ print('x shape:', x_train.shape)
 
 model = Sequential()
 # Shape is (batch_size, sentence_length)
-model.add(Conv1D(nb_filter=num_filters[0], filter_length=2, input_shape=(None, 2)))
+model.add(Conv1D(nb_filter=num_filters[0], filter_length=2, data_format='channels_first', input_shape=(2, None)))
 model.add(Activation('relu'))
 #model.add(MaxPooling1D(pool_size=num_features//batch_size, padding='valid'))
 #model.add(Activation('relu'))
@@ -122,9 +134,9 @@ model.add(Activation('relu'))
 #model.add(Activation('relu'))
 model.add(AveragePooling1D(pool_size=int(num_features), padding='same'))
 model.add(Activation('relu'))
-model.add(Bidirectional(LSTM(hidden_size)))
+#model.add(Bidirectional(LSTM(hidden_size)))
 model.add(Dropout(0.3))
-model.add(BatchNormalization())
+#model.add(BatchNormalization())
 #model.add(Dense(2048, activation='relu'))
 #model.add(Dense(1024, activation='relu'))
 model.add(Dense(512, activation='relu'))
